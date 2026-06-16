92da83af8528ab6e0d8b38b8e8bfe79706d16b5e
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-06-16 16:28:41 +0530

    Implement Clerk authentication with user onboarding and update user schema

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Button>Test</Button>
+      <UserButton />
     </div>
   );
 }
